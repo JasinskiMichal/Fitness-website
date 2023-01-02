d09bbd348b70c514c18e245c69e10a6d222e284f
MJ <Jasinski.M@protonmail.com>
2023-01-02 12:54:12 +0100

    comit full website

---
+++ js/script.js
@@ -0,0 +1,56 @@
+//Mobile navigation
+const btnNavEl = document.querySelector(".nav-mobile__icon");
+const headerEl = document.querySelector(".nav");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav--checked");
+});
+
+// //Smooth scrolling animation
+// const allLinks = document.querySelectorAll(".nav__link");
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+
+//     if (href == "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
+//     if (link.classList.contains("nav__link"))
+//       headerEl.classList.toggle("nav--checked");
+//   });
+// });
+
+// ///////////////////////////////////////////////////////////
+// // Sticky navigation
+
+// const section = document.querySelector(".background-img");
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
+
+//     if (ent.isIntersecting) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-125px",
+//   }
+// );
+// obs.observe(section);
